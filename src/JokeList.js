5108c5fe3ebeaaa5b57a5632e584fa4520191a8e
Jonathan <hj.punzalan@hotmail.com>
2019-05-21 07:15:39 +0800

    added JokeList component

---
+++ src/JokeList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class JokeList extends Component {
+	static defaultProps = {
+		numJokesToGet: 10
+	};
+
+	constructor(props) {
+		super(props);
+		this.state = { jokes: [] };
+	}
+
+	async componentDidMount() {
+		// Load jokes
+		let jokes = [];
+		while (jokes.length < this.props.numJokesToGet) {
+			let res = await axios.get('https://icanhazdadjoke.com/', {
+				headers: { Accept: 'application/json' }
+			});
+			jokes.push(res.data.joke);
+		}
+		this.setState({ jokes: jokes });
+	}
+
+	render() {
+		return (
+			<div className="JokeList">
+				<h1>Dad Jokes</h1>
+				<div className="JokesList-jokes">
+					{this.state.jokes.map(j => {
+						return <div>{j}</div>;
+					})}
+				</div>
+			</div>
+		);
+	}
+}

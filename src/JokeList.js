e5ea4bb968a8bf3e741cec2c680a16576cf913c0
Jonathan <hj.punzalan@hotmail.com>
2019-05-21 08:20:22 +0800

    Jokelist styling done except for button

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './_JokeList.scss';
 
 export default class JokeList extends Component {
 	static defaultProps = {
@@ -26,8 +27,18 @@ export default class JokeList extends Component {
 	render() {
 		return (
 			<div className="JokeList">
-				<h1>Dad Jokes</h1>
-				<div className="JokesList-jokes">
+				<div className="JokeList__sidebar">
+					<h1 className="JokeList__sidebar-title">
+						<span>Dad</span> Jokes
+					</h1>
+					<img
+						src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+						alt="zany face"
+						className="JokeList__sidebar-image"
+					/>
+					<button className="JokeList__sidebar-getmore">New Jokes</button>
+				</div>
+				<div className="JokeList__jokes">
 					{this.state.jokes.map(j => {
 						return <div>{j}</div>;
 					})}
